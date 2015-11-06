bbbaf00935a2bddf62930b6a8710b3b46d1c0f6f
Jeremy Evans <jeremy.n.evans@gmail.com>
2015-11-06 18:56:58 +0000

    trying home.ejs again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index');
+    response.render('home');
   //response.sendFile(__dirname + '/views/home.html');
 });
 
