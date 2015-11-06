838987b2faf8caf0ca437834d8af39dfbd2e388a
Jeremy Evans <jeremy.n.evans@gmail.com>
2015-11-06 18:47:01 +0000

    replaced sendfile with sendFile

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/views/home.html');
+  response.sendFile(__dirname + '/views/home.html');
 });
 
 app.listen(app.get('port'), function() {
