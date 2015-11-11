19c1a2db3cc5f14f1a0cda1a903af8411331537d
Jeremy Evans <jeremy.n.evans@gmail.com>
2015-11-11 16:11:30 +0000

    Staging config vars temporarily included in branch-specific (will next add this file to .gitignore

--- public/js/branch-specific.js
+++ public/js/branch-specific.js
@@ -5,9 +5,12 @@ var firebaseTeams = 'https://explaain-v2.firebaseio.com';
 var thisAlgoliaIndex = 'cards';
 //var thisAlgoliaIndexTeams = 'cards';
 var initialCard = '-Jtu1T1dCkU7cgdVh-I9';
+
+//////Everything above here should no longer be needed!
+
 var initialIdentity = '-JyDOPaDi5sYbe5tbqbp';
 
 
-var FIREBASE_INSTANCE_OPEN = 'https://explaain-v2-1.firebaseio.com';
-var FIREBASE_INSTANCE_CLOSED = 'https://explaain-v2.firebaseio.com';
-var ALGOLIA_INDEX_OPEN = 'cards';
+var FIREBASE_INSTANCE_OPEN = 'https://explaain-v2-sandbox.firebaseio.com';
+var FIREBASE_INSTANCE_CLOSED = 'https://explaain-v2-sandbox.firebaseio.com';
+var ALGOLIA_INDEX_OPEN = 'cards-staging';
